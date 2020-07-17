eba737280c67d76643ebc791d3bf4f13aaf5a7d4
b0s01ar <paul mcb -18>
2020-07-17 16:09:44 -0500

    iterate a lot of test cases

--- src/main/java/bertsanders/demo/stream/PrimeNumberService.java
+++ src/main/java/bertsanders/demo/stream/PrimeNumberService.java
@@ -1,12 +1,30 @@
 package bertsanders.demo.stream;
 
 import java.math.BigInteger;
+import java.util.Random;
+
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class PrimeNumberService {
+  private Random randomizer;
+
+  @PostConstruct
+  public void init() {
+    randomizer = new Random();
+  }
+
   public BigInteger nthPrime(int input) {
+    log.info("Calculating the Nth Prime for " + input);
+    if (randomizer.nextInt(5) < 1) {
+      throw new RuntimeException("random failure");
+    }
+
     int primeCount = 0;
     BigInteger number = BigInteger.ONE;
     while (primeCount < input) {
--- src/test/java/bertsanders/demo/stream/ApplicationTests.java
+++ src/test/java/bertsanders/demo/stream/ApplicationTests.java
@@ -2,27 +2,26 @@ package bertsanders.demo.stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.binder.test.InputDestination;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.GenericMessage;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootTest
 @Import(TestChannelBinderConfiguration.class)
 class ApplicationTests {
@@ -33,16 +32,26 @@ class ApplicationTests {
   private OutputDestination outputDestination;
 
   @Test
-  void contextLoads() throws IOException {
+  void testRandomMessages() {
 
-      Random randomizer = new Random();
-      Message<Integer> message = new GenericMessage<>(randomizer.nextInt(100));
-      inputDestination.send(message);
+    Random randomizer = new Random();
+    IntStream.range(1, 20)
+        .map(i -> randomizer.nextInt(1000) + 1)
+        .forEach(this::performTest);
+  }
 
-      Message<byte[]> output = outputDestination.receive();
-      BigInteger primeNumber = new ObjectMapper().readValue(output.getPayload(), BigInteger.class);
+  @SneakyThrows
+  private void performTest(int messageInput) {
+    Message<Integer> message = new GenericMessage<>(messageInput);
+    inputDestination.send(message);
 
-      assertThat(primeNumber.isProbablePrime(10)).isTrue();
+    Message<byte[]> output = outputDestination.receive();
+    assertThat(output).as("Failed to receive a message").isNotNull();
+
+    BigInteger primeNumber = new ObjectMapper().readValue(output.getPayload(), BigInteger.class);
+    log.info(primeNumber.toString());
+
+    assertThat(primeNumber.isProbablePrime(10)).isTrue();
   }
 
 }
